9ee03b54a57087e844de46dba9db87bb89f88247
MavoCz <matous@sapho.com>
2021-12-14 21:18:18 +0100

    Day 14

---
+++ src/test/kotlin/net/voldrich/aoc21/Day14Test.kt
@@ -0,0 +1,40 @@
+package net.voldrich.aoc21
+
+import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.Test
+
+internal class Day14Test {
+    @Test
+    fun testExample() {
+        val day = Day14()
+        day.input = TestInput("""
+            NNCB
+
+            CH -> B
+            HH -> N
+            CB -> H
+            NH -> C
+            HB -> C
+            HC -> B
+            HN -> C
+            NN -> C
+            BH -> H
+            NC -> B
+            NB -> B
+            BN -> B
+            BB -> N
+            BC -> B
+            CC -> N
+            CN -> C
+        """, true)
+        assertEquals(1588L, day.task1())
+        assertEquals(2188189693529L, day.task2())
+    }
+
+    @Test
+    fun testReal() {
+        val day = Day14()
+        assertEquals(3118L, day.task1())
+        assertEquals(4332887448171L, day.task2())
+    }
+}
